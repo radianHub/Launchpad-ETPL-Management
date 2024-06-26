147728328a25f4fcb13aabadc9c6e36911bbb6bd
Eric Sanchez <109996892+Eric-Salvador-Sanchez@users.noreply.github.com>
2024-06-26 12:11:05 -0600

    Scripts, initial

---
+++ scripts/Automations/promote.sh
@@ -0,0 +1,5 @@
+sf package version promote -p $1 --json
+
+## TODO sf package version list -p PACKAGE ID HERE -c 0 --verbose --json
+
+echo "Install Link: https://login.salesforce.com/packaging/installPackage.apexp?p0=$1"
